13b13bedce967aeac4fc872132b7f25147474dcb
vincent oh <vincent.form@gmail.com>
2023-01-12 17:42:25 +0800

    slight edits

--- webapp/frontend/src/components/simulate/Simulate.tsx
+++ webapp/frontend/src/components/simulate/Simulate.tsx
@@ -1,5 +1,5 @@
 import { DateTimePicker, LoadingButton, LocalizationProvider } from '@mui/lab';
-import AdapterDateFns from '@mui/x-date-pickers/AdapterDateFns';
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
 import { Alert, Button, FormControl, FormHelperText, InputLabel, MenuItem, OutlinedInput, Select, TextField } from '@mui/material';
 import Box from '@mui/material/Box';
 import { API } from 'aws-amplify';
